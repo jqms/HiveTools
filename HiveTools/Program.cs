3aac0c2ed50506ad5caf43531df5e0d6373cffa4
Rosie <jamsowo@outlook.com>
2023-01-13 01:59:33 +0000

    fixed leaderboard shit :) (thanks onix ily)

--- HiveTools/Program.cs
+++ HiveTools/Program.cs
@@ -9,19 +9,16 @@ namespace hiveTools
 {
     class StatChecker
     {
-        public static void print(string str) // lets not talk about this
-        {
+        public static void print(string str) {
             Console.WriteLine(str);
         }
-        public static void createOption(string option, string description)
-        {
+        public static void createOption(string option, string description) {
             Console.Write("[");
             Console.Write(option, Color.MediumBlue);
             Console.Write("] ");
             Console.WriteLine(description, Color.White);
         }
-        public static void getStats(string username, string gamemode)
-        {
+        public static void getStats(string username, string gamemode) {
             // Fix gamemode and username for URL
             username = username.Replace(" ", "%20").ToLower().Trim();
             gamemode = gamemode.ToLower().Trim();
@@ -35,41 +32,39 @@ namespace hiveTools
             var json = JObject.Parse(data);
             Console.WriteLine(json);
         }
-        // public static void getLeaderboard(string gamemode, bool monthly)
-        // {
-        //     // Fix gamemode for URL
-        //     gamemode = gamemode.ToLower().Trim();
-        //     // Get the stats from the API
-        //     string url = "";
-        //     if (monthly == true)
-        //     {
-        //         url = "https://api.playhive.com/v0/game/monthly/" + gamemode;
-        //         Console.WriteLine(url);
-        //     }
-        //     else
-        //     {
-        //         url = "https://api.playhive.com/v0/game/all/" + gamemode;
-        //         Console.WriteLine(url);
-        //     }
-        //     WebClient client = new WebClient();
-        //     string data = client.DownloadString(url);
-        //     Console.WriteLine(data);
+        public static void getLeaderboard(string gamemode, bool monthly) {
+            // Fix gamemode for URL
+            gamemode = gamemode.ToLower().Trim();
+            // Get the stats from the API
+            string url = "";
+            if (monthly == true) {
+                url = "https://api.playhive.com/v0/game/monthly/" + gamemode;
+                Console.WriteLine(url);
+            }
+            else {
+                url = "https://api.playhive.com/v0/game/all/" + gamemode;
+                Console.WriteLine(url);
+            }
+            WebClient client = new WebClient();
+            string data = client.DownloadString(url);
 
-        //     string json = (string)JArray.Parse(data);
-        //     print(json);
-        // }
-        public static void br()
-        {
+            // Parse the JSON
+            var json = JArray.Parse(data);
+            for (int i = 0; i < json.Count; i++)
+            {
+                Console.WriteLine(json[i]["human_index"]);
+                Console.WriteLine(json[i]["username"]);
+                br();
+            }
+        }
+        public static void br() {
             Console.WriteLine(" ");
         }
-        public static void Uptime(string address)
-        {
-            try
-            {
+        public static void Uptime(string address) {
+            try {
                 Ping susPing = new Ping();
                 PingReply response = susPing.Send(address, 1000);
-                if (response != null)
-                {
+                if (response != null) {
                     br();
                     Console.Write("Ping: ", Color.Blue);
                     Console.Write(response.RoundtripTime, Color.Green);
@@ -81,42 +76,34 @@ namespace hiveTools
                     Console.WriteLine(response.Address, Color.Green);
                 }
             }
-            catch
-            {
+            catch {
                 Console.WriteLine("\nUnable to ping!", Color.Red);
             }
             print("\nPress any key to go back.");
             Console.ReadKey();
         }
-        public static string selectRegion(string logo)
-        {
+        public static string selectRegion(string logo) {
             Console.Clear();
             Console.WriteLine(logo, Color.DarkRed);
             print("What region are you on? (NA/EU/AS)");
             string region = Console.ReadLine();
-            if (region == "NA")
-            {
+            if (region == "NA") {
                 return "ca.hivebedrock.network";
             }
-            else if (region == "EU")
-            {
+            else if (region == "EU") {
                 return "fr.hivebedrock.network";
             }
-            else if (region == "AS")
-            {
+            else if (region == "AS") {
                 return "sg.hivebedrock.network";
             }
-            else if (region == "AU")
-            {
+            else if (region == "AU") {
                 return "au.hivebedrock.network";
             }
-            else
-            {
+            else {
                 return "Invalid Region! (NA/EU/AS)";
             }
         }
-        static void Main()
-        {
+        static void Main() {
         start:
             Console.Title = "Hive Tools";
             Console.Clear();
@@ -132,21 +119,18 @@ namespace hiveTools
             createOption("10", "Exit");
 
             string input = Console.ReadLine();
-            if (input == "1")
-            {
+            if (input == "1") {
                 // Get the username and gamemode
                 Console.Write("Enter a username: ");
                 string username = Console.ReadLine();
-                if (username == "")
-                {
+                if (username == "") {
                     Console.WriteLine("Please enter a username: ");
                     Console.ReadLine();
                     return;
                 }
                 Console.Write("Enter a gamemode: ");
                 string gamemode = Console.ReadLine();
-                if (gamemode == "")
-                {
+                if (gamemode == "") {
                     Console.WriteLine("Please enter a gamemode: ");
                     Console.ReadLine();
                     return;
@@ -157,24 +141,36 @@ namespace hiveTools
                 Console.ReadKey();
                 goto start;
             }
-            if (input == "2")
-            {
-                print("Leaderboards are currently unsupported due to stupid json errors");
+            if (input == "2") {
+                Console.Write("Enter a gamemode: ");
+                string gamemode = Console.ReadLine();
+                if (gamemode == "") {
+                    Console.WriteLine("Please enter a gamemode: ");
+                    Console.ReadLine();
+                    return;
+                }
+                Console.Write("Monthly? (true/false): ");
+                string monthly = Console.ReadLine();
+                if (monthly == "true" || monthly == "false") {
+                    getLeaderboard(gamemode, bool.Parse(monthly));
+                }
+                else {
+                    Console.WriteLine("Please enter true or false: ");
+                    Console.ReadLine();
+                    return;
+                }
                 Console.WriteLine("Press any key to go back.");
                 Console.ReadKey();
                 goto start;
             }
-            if (input == "3")
-            {
+            if (input == "3") {
                 Uptime(selectRegion(logo));
                 goto start;
             }
-            if (input == "10")
-            {
+            if (input == "10") {
                 Environment.Exit(0);
             }
-            else
-            {
+            else {
                 print("Invalid key!");
                 Thread.Sleep(1000);
                 goto start;
